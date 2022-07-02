0462ebcc72545007baa48b09ad7774bc17b8663e
veevee weewee <vwang112@gmail.com>
2022-07-02 18:34:23 -0400

    initialize weights and biases

--- NeuralNetworkFramwork.py
+++ NeuralNetworkFramwork.py
@@ -4,10 +4,14 @@ import numpy as np
 class NeuralNetworkFramework:
     def __init__(self):
         self.weights = {
-            
+            1 : np.random.rand(16, 784),
+            2: np.random.rand(16, 16),
+            3: np.random.rand(16, 10)
         }
         self.biases = {
-
+            1: np.random.rand(16),
+            2: np.random.rand(16),
+            3: np.random.rand(10)
         }
 
     def sigmoid(x):
