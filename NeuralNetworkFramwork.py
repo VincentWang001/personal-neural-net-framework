a3c883afcfc75cb2a64edf94a08bca807418b267
veevee weewee <vwang112@gmail.com>
2022-07-03 23:05:20 -0400

    completed first draft backprop, added training test for one image

--- NeuralNetworkFramwork.py
+++ NeuralNetworkFramwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+from keras.datasets import mnist
 
 #3 layers, 784 -> 16 -> 16 -> 10
 class NeuralNetworkFramework:
@@ -7,7 +8,7 @@ class NeuralNetworkFramework:
         self.W = {
             1 : np.random.rand(16, 784),
             2: np.random.rand(16, 16),
-            3: np.random.rand(16, 10)
+            3: np.random.rand(10, 16)
         }
 
         #biases
@@ -16,10 +17,14 @@ class NeuralNetworkFramework:
             2: np.random.rand(16),
             3: np.random.rand(10)
         }
-        self.learning_rate = 0.05
+        self.learning_rate = 100
         self.num_layers = 3
         self.batch_size = 64
-
+        (train_X, train_y), (test_X, test_y) = mnist.load_data()
+        self.train_X = train_X
+        self.train_y = train_y
+        self.test_X = test_X
+        self.test_y = test_y
     
     #for 1 image
     def train(self, input_vec, digit):
@@ -34,7 +39,7 @@ class NeuralNetworkFramework:
         z[3] = self.W[3] @ a[2] + self.b[3] # 10 x 1
         a[3] = self.sigmoid(z[3]) # 10 x 1, output
         l = self.loss(a[3], y)
-
+        print("loss:", l)
         #backward pass
         dz = {}
         da = {}
@@ -62,15 +67,15 @@ class NeuralNetworkFramework:
         return (dW, db)
     
     #returns a length 10 vector of all 0s except the ith element, signifying that digit i is the correct classification
-    def onehot(self, i):
+    def one_hot(self, i):
         ret = np.zeros(10)
         ret[i] = 1
         return ret
     
     def update_weights_and_biases(self, dW, db):
-        for i in range(self.num_layers):
-            self.W[i] -= self.learning_rate * dW
-            self.b[i] -= self.learning_rate * db
+        for i in range(1, self.num_layers + 1):
+            self.W[i] -= self.learning_rate * dW[i]
+            self.b[i] -= self.learning_rate * db[i]
     
     #sigmoid function
     def sigmoid(self, x):
@@ -78,11 +83,27 @@ class NeuralNetworkFramework:
 
     #derivative of sigmoid function  
     def d_sigmoid(self, x):
-        sig = self.sigmoid(self, x)
+        sig = self.sigmoid(x)
         return sig * (1 - sig)
 
     #loss function
     def loss(self, actual, expected):
-        return 0.5 * (actual - expected) ** 2
+        return sum(0.5 * (actual - expected) ** 2)
+
+
+from matplotlib import pyplot as plt
 
 nn = NeuralNetworkFramework()
+
+#print(nn.train_X[0].flatten().shape)
+input = nn.train_X[0].flatten()
+for i in range(30):
+    dW, db = nn.train(input, 5)
+    #for i in range(1, 4):
+        #print(db[i].shape)
+        #print(nn.b[i].shape)
+    nn.update_weights_and_biases(dW, db)
+# for i in range(9):  
+#     plt.subplot(330 + 1 + i)
+#     plt.imshow(nn.train_X[i], cmap=plt.get_cmap('gray'))
+#     plt.show()
